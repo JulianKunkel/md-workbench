e385f2228936071b7ef08a756f4f23a40241f5d4
Julian Kunkel <juliankunkel@googlemail.com>
2016-12-09 09:51:26 +0100

    Updated plugin to new interface.

--- plugins/md-mpi.c
+++ plugins/md-mpi.c
@@ -77,28 +77,12 @@ static int finalize(){
 }
 
 
-static int prepare_testdir(char * dir){
-  if (use_posix_dirs){
-    return mkdir(dir, 0755);
-  }else if(use_existing_dirs){
-    // check if the directories exist ?
-    return MD_NOOP;
-  }
-  return MD_NOOP;
-}
-
-static int purge_testdir(char * dir){
-  if (use_posix_dirs){
-    return rmdir(dir);
-  }
-  return MD_NOOP;
-}
-
-static int create_rank_dir(char * filename, char * prefix, int rank){
-  if (show_hint_list && rank == 0){
+static int prepare_global(){
+  if (show_hint_list){
     MPI_Info einfo;
     MPI_File fh;
-    sprintf(filename, "%s/dummy", prefix);
+    char filename[4096];
+    sprintf(filename, "%s/dummy", dir);
     int ret = MPI_File_open(MPI_COMM_SELF, filename, MPI_MODE_DELETE_ON_CLOSE | MPI_MODE_CREATE | MPI_MODE_WRONLY, info, & fh);
     MPI_File_get_info(fh, & einfo);
     MPI_File_close(& fh);
@@ -124,49 +108,47 @@ static int create_rank_dir(char * filename, char * prefix, int rank){
   }
 
   if (use_posix_dirs){
-    sprintf(filename, "%s/%d", prefix, rank);
+    return mkdir(dir, 0755);
+  }else if(use_existing_dirs){
+    // check if the directories exist ?
+    return MD_NOOP;
+  }
+  return MD_NOOP;
+}
+
+static int purge_global(){
+  if (use_posix_dirs){
+    return rmdir(dir);
+  }
+  return MD_NOOP;
+}
+
+static int def_dset_name(char * out_name, int n, int d){
+  sprintf(out_name, "%s/%d_%d", dir, n, d);
+  return MD_SUCCESS;
+}
+
+static int def_obj_name(char * out_name, int n, int d, int i){
+  sprintf(out_name, "%s/%d_%d/file-%d", dir, n, d, i);
+  return MD_SUCCESS;
+}
+
+static int create_dset(char * filename){
+  if (use_posix_dirs){
     return mkdir(filename, 0755);
   }
   return MD_NOOP;
 }
 
-static int rm_rank_dir(char * filename, char * prefix, int rank){
+static int rm_dset(char * filename){
   if (use_posix_dirs){
-    sprintf(filename, "%s/%d", prefix, rank);
     return rmdir(filename);
   }
   return MD_NOOP;
 }
 
-
-static int create_dir(char * filename, char * prefix, int rank, int iteration){
-  if (use_posix_dirs){
-    sprintf(filename, "%s/%d/%d", prefix, rank, iteration);
-    return mkdir(filename, 0755);
-  }
-  return MD_NOOP;
-}
-
-static int rm_dir(char * filename, char * prefix, int rank, int iteration){
-  if (use_posix_dirs){
-    sprintf(filename, "%s/%d/%d", prefix, rank, iteration);
-    return rmdir(filename);
-  }
-  return MD_NOOP;
-}
-
-#define CREATE_FILENAME \
-  if (use_posix_dirs || use_existing_dirs){ \
-    sprintf(filename, "%s/%d/%d/file-%d", prefix, rank, dir, iteration);\
-  }else{\
-    sprintf(filename, "%s/%d_%d_%d", prefix, rank, dir, iteration);\
-  }
-
-
-static int write_file(char * filename, char * buf, size_t file_size,  char * prefix, int rank, int dir, int iteration){
+static int write_obj(char * filename, char * buf, size_t file_size){
   int ret;
-  CREATE_FILENAME
-
   MPI_File fh;
   ret = MPI_File_open(MPI_COMM_SELF, filename, MPI_MODE_CREATE | MPI_MODE_WRONLY, info, & fh);
   if (ret != MPI_SUCCESS){
@@ -187,10 +169,8 @@ static int write_file(char * filename, char * buf, size_t file_size,  char * pre
 
 
 
-static int read_file(char * filename, char * buf, size_t file_size,  char * prefix, int rank, int dir, int iteration){
+static int read_obj(char * filename, char * buf, size_t file_size){
   int ret;
-  CREATE_FILENAME
-
   MPI_File fh;
   ret = MPI_File_open(MPI_COMM_SELF, filename, MPI_MODE_RDONLY, info, & fh);
   if (ret != MPI_SUCCESS){
@@ -209,10 +189,8 @@ static int read_file(char * filename, char * buf, size_t file_size,  char * pref
   return MD_SUCCESS;
 }
 
-static int stat_file(char * filename, char * prefix, int rank, int dir, int iteration, int file_size){
+static int stat_obj(char * filename, size_t file_size){
   int ret;
-  CREATE_FILENAME
-
   MPI_File fh;
   ret = MPI_File_open(MPI_COMM_SELF, filename, MPI_MODE_RDONLY, info, & fh);
   if (ret != MPI_SUCCESS){
@@ -228,9 +206,7 @@ static int stat_file(char * filename, char * prefix, int rank, int dir, int iter
   return MD_SUCCESS;
 }
 
-static int delete_file(char * filename, char * prefix, int rank, int dir, int iteration){
-  CREATE_FILENAME
-
+static int delete_obj(char * filename){
   int ret = MPI_File_delete(filename, MPI_INFO_NULL);
   return ret == MPI_SUCCESS ? MD_SUCCESS : MD_ERROR_UNKNOWN;
 }
@@ -243,14 +219,16 @@ struct md_plugin md_plugin_mpi = {
   get_options,
   initialize,
   finalize,
-  prepare_testdir,
-  purge_testdir,
-  create_rank_dir,
-  rm_rank_dir,
-  create_dir,
-  rm_dir,
-  write_file,
-  read_file,
-  stat_file,
-  delete_file
+  prepare_global,
+  purge_global,
+
+  def_dset_name,
+  create_dset,
+  rm_dset,
+
+  def_obj_name,
+  write_obj,
+  read_obj,
+  stat_obj,
+  delete_obj
 };
