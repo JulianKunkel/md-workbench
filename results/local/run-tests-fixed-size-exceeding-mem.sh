d0ea1c536905b988975d34b1bbdf8cea4373723e
Julian M. Kunkel <juliankunkel@googlemail.com>
2016-12-15 20:48:08 +0100

    Larger working set fixed for sandy9.

--- results/local/run-tests-fixed-size-exceeding-mem.sh
+++ results/local/run-tests-fixed-size-exceeding-mem.sh
@@ -24,7 +24,7 @@ function runBenchmark(){
 			for N in 1 2 3 4 5 6 7 8 10 12 15 ; do
 				FILE=/home/kunkel/benchmarks/md-real-io/$NAME-$fs-${M}bigger--$N.txt
 				if [[ ! -e $FILE ]] ; then
-					mpiexec -n $N $BENCH -P=$((500000/$N)) -I=10000 -i=posix -m=$M -R=5 --process-reports -- -D=/mnt/test/out > $FILE 2>&1
+					mpiexec -n $N $BENCH -P=$((10000/$N)) -D=50 -I=10000 -i=posix -m=$M -R=5 --process-reports -- -D=/mnt/test/out > $FILE 2>&1
 				fi
 			done
 		done
