1383d95a7b065da3bec613c2ac064adf3b755026
Julian M. Kunkel <juliankunkel@googlemail.com>
2017-07-08 22:13:19 +0200

    Analysis: more output.

--- analyze-stats-file.R
+++ analyze-stats-file.R
@@ -8,8 +8,8 @@ input = args[1]
 
 d = read.csv(input)
 
-print(summary(d$runtime))
 print(sprintf("quantiles 0.1: %e 0.9: %e", quantile(d$runtime, 0.1), quantile(d$runtime,0.9)))
+print(summary(d$runtime))
 
 p = ggplot(d, aes(x=time,y=runtime)) + geom_point(alpha=.4, size=1) + xlab("time") + ylab("runtime") + scale_y_log10()
 ggsave(sprintf("%s-timeline.pdf", input), plot=p)
--- analyze-stats-files.R
+++ analyze-stats-files.R
@@ -8,20 +8,34 @@ input = args[1]
 
 # now we have to read three files
 d1 = read.csv(sprintf("%s-create-0.csv", input))
+
 d1$type = as.factor("create")
+print(sprintf("create quantiles 0.1: %e 0.9: %e", quantile(d1$runtime, 0.1), quantile(d1$runtime,0.9)))
+print(summary(d1$runtime))
+
 d2 = read.csv(sprintf("%s-read-0.csv", input))
 d2$type = as.factor("read")
+print(sprintf("read quantiles 0.1: %e 0.9: %e", quantile(d2$runtime, 0.1), quantile(d2$runtime,0.9)))
+print(summary(d2$runtime))
+
 d3 = read.csv(sprintf("%s-delete-0.csv", input))
 d3$type = as.factor("delete")
+print(sprintf("delete quantiles 0.1: %e 0.9: %e", quantile(d3$runtime, 0.1), quantile(d3$runtime,0.9)))
+print(summary(d3$runtime))
+
 d4 = read.csv(sprintf("%s-stat-0.csv", input))
 d4$type = as.factor("stat")
+print(sprintf("stat quantiles 0.1: %e 0.9: %e", quantile(d4$runtime, 0.1), quantile(d4$runtime,0.9)))
+print(summary(d4$runtime))
+
+
 
 d = rbind(d1, d2, d3, d4)
 
 p = ggplot(d, aes(x=time,y=runtime,col=type)) + geom_point(alpha=.4, size=1) + xlab("time") + ylab("runtime") + scale_y_log10() +   theme(legend.position="bottom")
 ggsave(sprintf("%s-timeline.pdf", input), plot=p)
 
-p = ggplot(d, aes(x=runtime,fill=type)) + geom_density(alpha=.2)  + scale_x_log10() +   theme(legend.position="bottom") # +  geom_histogram(aes(y=..density..), binwidth=.5, colour="black", fill="white")
+p = ggplot(d, aes(x=runtime,col=type)) + geom_density(alpha=.2)  + scale_x_log10() +   theme(legend.position="bottom") # +  geom_histogram(aes(y=..density..), binwidth=.5, colour="black", fill="white")
 ggsave(sprintf("%s-density.pdf", input), plot=p)
 
 p = ggplot(d, aes(x=type, y=runtime, fill=type)) + xlab("") + geom_boxplot() + scale_y_log10() +   theme(legend.position="bottom")
