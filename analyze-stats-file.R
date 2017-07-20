590030081ab1813ab38f8d705fe2a922389d0cf6
Julian M. Kunkel <juliankunkel@googlemail.com>
2017-07-20 12:30:13 +0200

    Plot various diagrams.

--- analyze-stats-file.R
+++ analyze-stats-file.R
@@ -12,15 +12,15 @@ print(sprintf("quantiles 0.1: %e 0.9: %e", quantile(d$runtime, 0.1), quantile(d$
 print(summary(d$runtime))
 
 p = ggplot(d, aes(x=time,y=runtime)) + geom_point(alpha=.4, size=1) + xlab("time") + ylab("runtime") + scale_y_log10()
-ggsave(sprintf("%s-timeline.pdf", input), plot=p)
+ggsave(sprintf("%s-timeline.pdf", input), plot=p, width=8, height=5)
 
 p = ggplot(d, aes(x=runtime)) + geom_histogram(aes(y=..density..), binwidth=.5, colour="black", fill="white") + geom_density(alpha=.2, fill="#7777FF")  + scale_x_log10()
-ggsave(sprintf("%s-density.pdf", input), plot=p)
+ggsave(sprintf("%s-density.pdf", input), plot=p, width=8, height=5)
 
 p = ggplot(d, aes(x="", y=runtime, fill=runtime)) + xlab("") + geom_boxplot() + scale_y_log10()
-ggsave(sprintf("%s-runtime.pdf", input), plot=p)
+ggsave(sprintf("%s-runtime.pdf", input), plot=p, width=8, height=5)
 
 d = d[ order(d$runtime),]
 d$pos = (1:nrow(d))/nrow(d)
 p = ggplot(d, aes(x=pos, y=runtime)) + geom_point(alpha=.4, size=1) + xlab("fraction of measurements") + ylab("runtime") + scale_y_log10()
-ggsave(sprintf("%s-waittimes.pdf", input), plot=p)
+ggsave(sprintf("%s-waittimes.pdf", input), plot=p, width=8, height=5)
--- analyze-stats-files.R
+++ analyze-stats-files.R
@@ -5,25 +5,26 @@ library(ggplot2)
 options(echo=FALSE)
 args = commandArgs(trailingOnly = TRUE)
 input = args[1]
+process = 1
 
 # now we have to read three files
-d1 = read.csv(sprintf("%s-create-0.csv", input))
+d1 = read.csv(sprintf("%s-create-%d.csv", input, process))
 
 d1$type = as.factor("create")
 print(sprintf("create quantiles 0.1: %e 0.9: %e", quantile(d1$runtime, 0.1), quantile(d1$runtime,0.9)))
 print(summary(d1$runtime))
 
-d2 = read.csv(sprintf("%s-read-0.csv", input))
+d2 = read.csv(sprintf("%s-read-%d.csv", input, process))
 d2$type = as.factor("read")
 print(sprintf("read quantiles 0.1: %e 0.9: %e", quantile(d2$runtime, 0.1), quantile(d2$runtime,0.9)))
 print(summary(d2$runtime))
 
-d3 = read.csv(sprintf("%s-delete-0.csv", input))
+d3 = read.csv(sprintf("%s-delete-%d.csv", input, process))
 d3$type = as.factor("delete")
 print(sprintf("delete quantiles 0.1: %e 0.9: %e", quantile(d3$runtime, 0.1), quantile(d3$runtime,0.9)))
 print(summary(d3$runtime))
 
-d4 = read.csv(sprintf("%s-stat-0.csv", input))
+d4 = read.csv(sprintf("%s-stat-%d.csv", input, process))
 d4$type = as.factor("stat")
 print(sprintf("stat quantiles 0.1: %e 0.9: %e", quantile(d4$runtime, 0.1), quantile(d4$runtime,0.9)))
 print(summary(d4$runtime))
@@ -33,21 +34,21 @@ print(summary(d4$runtime))
 d = rbind(d1, d2, d3, d4)
 
 p = ggplot(d, aes(x=time,y=runtime,col=type)) + geom_point(alpha=.4, size=1) + xlab("time") + ylab("runtime") + scale_y_log10() +   theme(legend.position="bottom")
-ggsave(sprintf("%s-timeline.pdf", input), plot=p)
+ggsave(sprintf("%s-timeline-%d.pdf", input, process), plot=p, width=8, height=5)
 
 p = ggplot(d, aes(x=runtime,col=type)) + geom_density(alpha=.2)  + scale_x_log10() +   theme(legend.position="bottom") # +  geom_histogram(aes(y=..density..), binwidth=.5, colour="black", fill="white")
-ggsave(sprintf("%s-density.pdf", input), plot=p)
+ggsave(sprintf("%s-density-%d.pdf", input, process), plot=p, width=8, height=5)
 
 p = ggplot(d, aes(x=type, y=runtime, fill=type)) + xlab("") + geom_boxplot() + scale_y_log10() +   theme(legend.position="bottom")
-ggsave(sprintf("%s-runtime.pdf", input), plot=p)
+ggsave(sprintf("%s-runtime-%d.pdf", input, process), plot=p, width=8, height=5)
 
 d = d[ order(d$runtime),]
 d$pos = (1:nrow(d))/nrow(d)
 p = ggplot(d, aes(x=pos, y=runtime,col=type)) + geom_point(alpha=.4, size=1) + xlab("fraction of measurements") + ylab("runtime") + scale_y_log10() +   theme(legend.position="bottom")
-ggsave(sprintf("%s-waittimes-all.pdf", input), plot=p)
+ggsave(sprintf("%s-waittimes-all-%d.pdf", input, process), plot=p, width=8, height=5)
 
 p = ggplot(d, aes(x=pos, y=runtime,col=type)) + geom_point(alpha=.4, size=1) + xlab("fraction of measurements") + ylab("runtime") + scale_y_log10() + facet_grid(type ~ .) + theme(legend.position="none")
-ggsave(sprintf("%s-waittimes.pdf", input), plot=p)
+ggsave(sprintf("%s-waittimes-%d.pdf", input, process), plot=p, width=8, height=5)
 
 
 d = d[ order(d$type, d$runtime),]
@@ -57,4 +58,4 @@ d$pos[(1*nrow(d1)+1):(2*nrow(d1))] = (1:nrow(d1))/nrow(d1)
 d$pos[(2*nrow(d1)+1):(3*nrow(d1))] = (1:nrow(d1))/nrow(d1)
 d$pos[(3*nrow(d1)+1):(4*nrow(d1))] = (1:nrow(d1))/nrow(d1)
 p = ggplot(d, aes(x=pos, y=runtime,col=type)) + geom_point(alpha=.4, size=1) + xlab("fraction of measurements") + ylab("runtime") + scale_y_log10() + facet_grid(type ~ .) + theme(legend.position="none")
-ggsave(sprintf("%s-waittimes-ind.pdf", input), plot=p)
+ggsave(sprintf("%s-waittimes-ind-%d.pdf", input, process), plot=p, width=8, height=5)
