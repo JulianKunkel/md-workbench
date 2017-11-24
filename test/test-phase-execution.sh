daa9477f929737ae0a77ca2113f1c8f2f533153e
Julian Kunkel <juliankunkel@googlemail.com>
2017-11-24 10:37:20 +0100

    Systemtest: Better description / example

--- test/test-phase-execution.sh
+++ test/test-phase-execution.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/bash -e
 
-ARGS="-w=600 -W -O=1 -I=2000 -D=1 -P=10000 -R=3 --process-reports -L=/tmp/1   -S=3901 --latency-all"
+# This little system test shows how to separate md-real execution into multiple phases
+# It does set various useful options like stonewalling, recording of traces
+
+ARGS="-w=10 -W -O=1 -I=2000 -D=1 -P=10000 -R=3 --process-reports -L=/tmp/1 -S=3901 --latency-all --run-info-file=mdreal-status"
+
 PLUGIN_ARGS="-- -D=md-testdir"
+RUN="mpiexec -n 2 ../build/src/md-real-io"
 
-./src/md-real-io $ARGS -1 $PLUGIN_ARGS
-./src/md-real-io $ARGS -2 $PLUGIN_ARGS
-./src/md-real-io $ARGS -2 $PLUGIN_ARGS
-./src/md-real-io $ARGS -3 $PLUGIN_ARGS
+$RUN $ARGS -1 $PLUGIN_ARGS
+$RUN $ARGS -2 $PLUGIN_ARGS
+$RUN $ARGS -2 $PLUGIN_ARGS
+$RUN $ARGS -3 $PLUGIN_ARGS
 
 
-./src/md-real-io $ARGS -1 $PLUGIN_ARGS
-./src/md-real-io $ARGS -2 --read-only $PLUGIN_ARGS
-./src/md-real-io $ARGS -2 $PLUGIN_ARGS
-./src/md-real-io $ARGS -3 $PLUGIN_ARGS
+$RUN $ARGS -1 $PLUGIN_ARGS
+$RUN $ARGS -2 --read-only $PLUGIN_ARGS
+$RUN $ARGS -2 $PLUGIN_ARGS
+$RUN $ARGS -3 $PLUGIN_ARGS
